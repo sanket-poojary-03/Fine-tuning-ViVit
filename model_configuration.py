5780f308c8c91e1c03443e1b4ec9a3c9b8614bf2
sanket-poojary-03 <164500111+sanket-poojary-03@users.noreply.github.com>
2024-07-27 22:10:40 +0530

    Add files via upload
    Configuring the model for custom dataset with 10 classes

---
+++ model_configuration.py
@@ -0,0 +1,30 @@
+import numpy as np
+from datasets import load_metric
+
+metric = load_metric("accuracy")
+def compute_metrics(p):
+    return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
+
+import torch
+def collate_fn(batch):
+    return {
+        'pixel_values': torch.stack([(torch.tensor(x['pixel_values']))  for x in batch]),
+        'labels': torch.tensor([x['labels'] for x in batch]),
+
+    }
+from transformers import  VivitConfig,VivitForVideoClassification
+
+labels = shuffled_dataset.features['labels'].names
+config = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
+config.num_classes=len(labels)
+config.id2label = {str(i): c for i, c in enumerate(labels)}
+config.label2id = {c: str(i) for i, c in enumerate(labels)}
+config.num_frames=10
+config.video_size= [32, 224, 224]
+
+model = VivitForVideoClassification.from_pretrained(
+   "google/vivit-b-16x2-kinetics400",
+   ignore_mismatched_sizes=True,
+    config=config,
+
+).to(device)
