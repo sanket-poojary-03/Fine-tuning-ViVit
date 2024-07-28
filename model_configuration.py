4d5236e852c32c946dc8f7a3bd722d042249c2e5
sanket-poojary-03 <164500111+sanket-poojary-03@users.noreply.github.com>
2024-07-28 08:41:13 +0530

    Update model_configuration.py

--- model_configuration.py
+++ model_configuration.py
@@ -1,6 +1,6 @@
 import numpy as np
 from datasets import load_metric
-
+from transformers import  VivitConfig,VivitForVideoClassification
 metric = load_metric("accuracy")
 def compute_metrics(p):
     return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
@@ -12,19 +12,16 @@ def collate_fn(batch):
         'labels': torch.tensor([x['labels'] for x in batch]),
 
     }
-from transformers import  VivitConfig,VivitForVideoClassification
+def initalise_model():
+ labels = shuffled_dataset.features['labels'].names
+ config = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
+ config.num_classes=len(labels)
+ config.id2label = {str(i): c for i, c in enumerate(labels)}
+ config.label2id = {c: str(i) for i, c in enumerate(labels)}
+ config.num_frames=10
+ config.video_size= [32, 224, 224]
 
-labels = shuffled_dataset.features['labels'].names
-config = VivitConfig.from_pretrained("google/vivit-b-16x2-kinetics400")
-config.num_classes=len(labels)
-config.id2label = {str(i): c for i, c in enumerate(labels)}
-config.label2id = {c: str(i) for i, c in enumerate(labels)}
-config.num_frames=10
-config.video_size= [32, 224, 224]
-
-model = VivitForVideoClassification.from_pretrained(
+ model = VivitForVideoClassification.from_pretrained(
    "google/vivit-b-16x2-kinetics400",
    ignore_mismatched_sizes=True,
-    config=config,
-
-).to(device)
+    config=config,).to(device)
